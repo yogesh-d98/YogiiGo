d217af14757e7cda3c49fd45cfd11de2826efb9e
yogesh-d98 <yogiiii1998@gmail.com>
2025-09-11 20:53:47 +0530

    feat(server): update routes index

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import authRoutes from "../modules/auth/auth.routes";
 import testRoutes from "./test.routes";
+import storeRoutes from "../modules/store/store.routes"
+import productRoutes from "../modules/product/product.routes";
+import categoryRoutes from "../modules/category/category.routes";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/test", testRoutes);
-
+router.use("/store", storeRoutes); 
+router.use("/product",productRoutes);
+router.use("/category",categoryRoutes);
 export default router;
