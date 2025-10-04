a01edb146367d3b78285e0f98e8f08f9515f6463
yogesh-d98 <yogiiii1998@gmail.com>
2025-10-05 02:26:29 +0530

    fleat(server): defining port

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,12 +3,12 @@ import { Server } from "socket.io";
 import app from "./app";
 import { initSockets } from "./sockets";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const httpServer = http.createServer(app);
 
 export const io = new Server(httpServer, {
-  cors: { origin: "*", methods: ["GET", "POST" , "PUT" , "DELETE" , "PATCH"] },
+  cors: { origin: "http://localhost:3000", methods: ["GET", "POST" , "PUT" , "DELETE" , "PATCH"] },
 });
 
 initSockets(io);
